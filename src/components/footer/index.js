32dc53ee996ca94ee16a7a7891ab63e2610040ea
GiaHung02 <phamgiahung02@gmail.com>
2026-01-16 22:47:43 +0700

    footer(styles): add footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,50 @@
+import Image from 'next/image';
 import styles from './styles.module.scss';
 
 export default function Index(params) {
     return (
         <>
-            <div className={styles.dashboard}>
-                <h1>this is footer</h1>
-            </div>
+            <footer className={styles.footer}>
+                <div className='container'>
+                    <div className={styles.footer_container}>
+                        <div className={styles.col_left}>
+                            <div>
+                                <Image src="/images/logo.svg" alt="Logo" width={233} height={82} />
+                            </div>
+                            <div>
+                                <p>It is our goal to provide age appropriate opportunity for every child enrolled in EduKid Kids Club enrichment classes.</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.col_right}>
+                            <h2>Quick Links</h2>
+                            <div className={styles.quick_links}>
+                                <ul>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Courses</a></li>
+                                    <li><a href="#">Shop</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                </ul>
+
+                                <ul>
+                                    <li><a href="#">Recently Viewed</a></li>
+                                    <li><a href="#">New Programs</a></li>
+                                    <li><a href="#">Curriculums</a></li>
+                                    <li><a href="#">Careers</a></li>
+                                    <li><a href="#">School stuff</a></li>
+                                </ul>
+
+                                <ul>
+                                    <li><Image src="/images/components/footer/address.svg" alt="Location" width={20} height={20} /> <a href="#">Westlands Building, Nairobi, KE</a></li>
+                                    <li><Image src="/images/components/footer/phone.svg" alt="Phone" width={20} height={20} /> <a href="#">012-345-6789</a></li>
+                                    <li><Image src="/images/components/footer/email.svg" alt="Email" width={20} height={20} /> <a href="#">edukidsupport@email.com</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </>
     );
 }
